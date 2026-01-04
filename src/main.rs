35338196ee008866387308e28d7c226f22cc7612
Stuart Chavez-Dundas <stuartdundas@pm.me>
2026-01-04 14:42:23 -0500

    Set app display name to M Reader

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let settings = settings::load_settings();
     let menu = menu::build_menu(&settings);
     let window = dioxus::desktop::WindowBuilder::new()
-        .with_title("MReader")
+        .with_title("M Reader")
         .with_min_inner_size(dioxus::desktop::LogicalSize::new(720.0, 520.0));
     let config = dioxus::desktop::Config::new()
         .with_menu(menu)
